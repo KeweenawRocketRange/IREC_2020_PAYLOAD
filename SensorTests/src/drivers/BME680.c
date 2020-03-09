5719914395f5d77c68415831c230dcfce0d8571f
ErikVH23 <eavanhor@mtu.edu>
2020-03-09 23:54:46 +0000

    added bme680 drivers

--- SensorTests/src/drivers/BME680.c
+++ SensorTests/src/drivers/BME680.c
@@ -4,3 +4,11 @@
 #include <math.h>
 #include <wiringPiI2C.h>
 #inlude "BME680.h"
+
+bme680_data bme;
+
+bme680_data* getRawData(int fd){
+		
+
+
+}
--- SensorTests/src/drivers/BME680.h
+++ SensorTests/src/drivers/BME680.h
@@ -1,1 +1,11 @@
 //BME680
+//
+//
+
+
+typdef struct{
+
+	int pressure;
+
+	int altitude;
+}bme680_data;
