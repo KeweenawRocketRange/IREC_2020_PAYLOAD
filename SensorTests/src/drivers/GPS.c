48b25555d5b1c8cce67da3d44444ea34f34b9ce7
Erik Van Horn <ErikVH23@users.noreply.github.com>
2020-03-17 20:31:55 -0400

    change

--- SensorTests/src/drivers/GPS.c
+++ SensorTests/src/drivers/GPS.c
@@ -23,7 +23,9 @@ char*  NMEA_STRING(char* cpOutput)
 
 	}
 
-	if((bufferSize = serialDataAvail(fd)) != -1 && strcmp((char)serialGetChar(fd),"$") == 0)
+	char* start = (char)serialGetChar(fd);
+	char* dollar = "$";
+	if((bufferSize = serialDataAvail(fd)) != -1 && strcmp(start,dollar) == 0)
 	{
 		//printf("Char available(1): %d\n",serialDataAvail(fd));
 		//printf("Char available(2): %d\n",serialDataAvail(fd));
