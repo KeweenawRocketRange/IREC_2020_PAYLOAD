9bf0295922dc7f0066e28df01880b0aea9c08e92
Erik Van Horn <ErikVH23@users.noreply.github.com>
2020-03-18 00:05:52 -0400

    added interrupt functionality

--- SensorTests/src/drivers/GPSV2.c
+++ SensorTests/src/drivers/GPSV2.c
@@ -1,9 +1,7 @@
 /*
-
   This is a revision of GPS.c
 
 */
-
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -15,10 +13,48 @@
 #include<regex.h>
 
 
+void NMEA_STRING()
+{
+
+	char NMEA [83];
+	int fd;
+
+	int bufferSize = 0;
+
+	if((fd = serialOpen("/dev/serial0",38400)) < 0){
+
+		fprintf(stdout, "Could not open file fd.");
+
+	}
+
+
+	if(serialDataAvail(fd) > 0)
+	{
+
+		for(int i=0; i<bufferSize+1; i++)
+		{
+			int val = serialGetchar(fd);
+			NMEA[i] = (char)val;
+		}
+		NMEA[82] = '\0';
+}
+	//char* cpOutput =(char *)malloc(sizeof(char)*83);
+
+
+	cpOutput = NMEA;
+
+  //nmeaParse(cpOutput);
+
+	printf("Raw Output:\n%s\n",NMEA);
+
+}
+
 void getCoord(){
 
   if(wiringPiSetup() < 0)
     fprintf(stderr, "Failed to setup wiringPi.%s"\n",strerror(errno) );
 
-    if( wiringPiISR)
+    if( wiringPiISR(PPS,INT_EDGE_FALLING, &NMEA_STRING) > 0){
+      fprintf(stderr, "Unable To setup ISR: %s\n",strerror(errno));
+    }
 }
