db72e74f1e0c6cefb1c592b95e21c32de50d6443
Erik Van Horn <ErikVH23@users.noreply.github.com>
2020-03-19 01:08:32 -0400

    Updated pin # for Pi

--- SensorTests/src/drivers/GPSV2.c
+++ SensorTests/src/drivers/GPSV2.c
@@ -1,7 +1,7 @@
 /*
   This is a revision of GPS.c
 
-*/
+*
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -12,6 +12,8 @@
 #include<errno.h>
 #include<regex.h>
 
+//GPIO 0, pin 11
+#define PPS 0
 
 void NMEA_STRING()
 {
@@ -19,17 +21,15 @@ void NMEA_STRING()
 	char NMEA [83];
 	int fd;
 
-	int bufferSize = 0;
+	//Size of typical NMEA message ( 83 characters )
+	int bufferSize = 83;
 
 	if((fd = serialOpen("/dev/serial0",38400)) < 0){
-
 		fprintf(stdout, "Could not open file fd.");
-
 	}
 
-
-	if(serialDataAvail(fd) > 0)
-	{
+	//If char available to be read, returns the # of available characters
+	if(serialDataAvail(fd) > 0){
 
 		for(int i=0; i<bufferSize+1; i++)
 		{
@@ -39,21 +39,21 @@ void NMEA_STRING()
 		NMEA[82] = '\0';
 }
 	//char* cpOutput =(char *)malloc(sizeof(char)*83);
-
-
 	cpOutput = NMEA;
-
   //nmeaParse(cpOutput);
-
 	printf("Raw Output:\n%s\n",NMEA);
 
 }
 
+
+
 void getCoord(){
 
+	//Sets up wiringPi
   if(wiringPiSetup() < 0)
     fprintf(stderr, "Failed to setup wiringPi.%s"\n",strerror(errno) );
 
+		//Sets up function interrupt
     if( wiringPiISR(PPS,INT_EDGE_FALLING, &NMEA_STRING()) > 0){
       fprintf(stderr, "Unable To setup ISR: %s\n",strerror(errno));
     }
