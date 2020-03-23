681a6be419655427b93b9c43af6d625008efaa89
Erik Van Horn <ErikVH23@users.noreply.github.com>
2020-03-22 20:40:30 -0500

    sdf

--- SensorTests/src/main.c
+++ SensorTests/src/main.c
@@ -1,22 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include"MPU6050Test.h"
-#include"/home/pi/C_Files/KRR/IREC_2020_Payload/SensorTests/src/drivers/GPS.h"
+#include"/home/pi/C_Files/KRR/IREC_2020_Payload/SensorTests/src/drivers/GPSV2.h"
 #define MPUADDR 0x68;
 
 int main(){
 
-	int mpuAddr = MPUADDR;	
-	setup(mpuAddr);
+		getCoord();
 
-	char* cpOut = ((char*)malloc(sizeof(char)*83));
-	for(int i = 0; i < 100; i++){
-		
-		NMEA_STRING(cpOut);
-		//printf("Accel: %f\n", read_word_2c(0x3B)/16384.0); 
-		
-	}
 
-	free(cpOut);
 	return 0;
 }
