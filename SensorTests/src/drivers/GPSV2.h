191c87c3a319f6fed39759ff9376d046fe5cc394
Erik Van Horn <ErikVH23@users.noreply.github.com>
2020-03-22 23:44:53 -0500

    sdf

--- SensorTests/src/drivers/GPSV2.h
+++ SensorTests/src/drivers/GPSV2.h
@@ -1,16 +1,7 @@
 
+double Longitude(double longitude);
+double Latitude(double latitude);
 void NMEA_STRING();
 void getCoord();
 void interruptTest();
-double Latitude(double latitude);
-double Longitude(double longitude);
 void nmeaParse(int length, char nmea[]);
-
-typedef struct{
-
-  double latitude;
-  double longitude;
-  char lat;
-  char lon;
-
-}GPS_COORD;
