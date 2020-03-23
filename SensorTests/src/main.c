a6858a9229b5a5f913c437761b8e0b25dc361d0b
Erik Van Horn <ErikVH23@users.noreply.github.com>
2020-03-23 00:20:04 -0500

    sdf

--- SensorTests/src/main.c
+++ SensorTests/src/main.c
@@ -10,7 +10,7 @@
 #include<stdlib.h>
 #include"/home/pi/C_Files/KRR/IREC_2020_Payload/SensorTests/src/drivers/MPU6050.h"
 #include"/home/pi/C_Files/KRR/IREC_2020_Payload/SensorTests/src/drivers/GPSV2.h"
-#define MPUADDR 0x68;
+#define MPUADDR 0x68
 
 int main(){
 
@@ -24,7 +24,7 @@ while(1){
 	accel = read_word_2c(ACCEL_X);
 
 	printf("ACCEL_X: %f\n",accel);
-	
+
 }
 
 	return 0;
