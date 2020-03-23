59748c5f8288e82eeb733d5b5bb5245931ca7dfe
Erik Van Horn <ErikVH23@users.noreply.github.com>
2020-03-23 00:18:32 -0500

    sdf

--- SensorTests/src/drivers/MPU6050.h
+++ SensorTests/src/drivers/MPU6050.h
@@ -3,12 +3,12 @@
 int setup(int MPUADDR);
 int read_word_2c(int addr);
 
-#define ACCEL_X 0x3B;
-#define ACCEL_Y 0x3D;
-#define ACCEL_Z 0x3F;
-#define GYRO_X  0x43;
-#define GYRO_Y  0x45;
-#define GYRO_Z  0x47;
+#define ACCEL_X 0x3B
+#define ACCEL_Y 0x3D
+#define ACCEL_Z 0x3F
+#define GYRO_X  0x43
+#define GYRO_Y  0x45
+#define GYRO_Z  0x47
 
 
 typdef struct {
